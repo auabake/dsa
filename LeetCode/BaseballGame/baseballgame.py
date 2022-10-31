7e9098b9fb37529a5cbd36dbdd90441c81a57394
Abubakar Njumwa <auabake@gmail.com>
2022-10-31 11:24:30 +0300

    Find the Town Judge

--- LeetCode/BaseballGame/baseballgame.py
+++ LeetCode/BaseballGame/baseballgame.py
@@ -1,14 +1,13 @@
-def calPoints(self, ops):
-      stack = []
-      for i in ops:
-         if i == "+":
-            first, second = stack[len(stack) - 1],
-            stack[len(stack) - 2]
-            stack.append(first + second)
-         elif i == "D":
-            stack.append(stack[-1] * 2)
-         elif i == "C":
-            stack.pop()
-         else:
-            stack.append(int(i))
-      return sum(stack)
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for i in operations:
+            if i == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif i == 'D':
+                stack.append(stack[-1] * 2)
+            elif i == 'C':
+                stack.pop()
+            else:
+                stack.append(int(i))
+        return sum(stack)
---
+++ LeetCode/FindtheTownJudge/FindtheTownJudge.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+
+        ingress = defaultdict(set)
+        egress =defaultdict(set)
+        for p, q in trust:
+            egress[p].add(q)
+            ingress[q].add(p)
+        for i in range(1, N+1):
+            if len(egress[i]) == 0 and len(ingress[i]) == N - 1:
+                return i
+        return -1
