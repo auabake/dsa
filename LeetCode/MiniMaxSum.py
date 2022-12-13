0f9cd14be47d8d00f0e923b62d52d8a9f2fbb1f7
Abubakar Njumwa <auabake@gmail.com>
2022-12-13 12:49:24 +0300

    optimization

--- LeetCode/MiniMaxSum.py
+++ LeetCode/MiniMaxSum.py
@@ -1,5 +1,5 @@
 """
-# Mini_Max_Sum 
+Mini_Max_Sum 
 
 Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
@@ -10,25 +10,10 @@ arr=[1,3,5,7,9]
 The minimum sum is 1+3+5+7=16 
 The maximum sum is 3+5+7+9=24
 
-The function prints
-16 24 
+The function prints 16 24 
 """
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'miniMaxSum' function below.
-#
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
 
 def miniMaxSum(arr):
-    # Write your code here
     arr.sort()
     k = sum(arr) # get the sum of the sorted arry 
     mn = k-arr[len(arr)-1] # minimum_sum = sum - the int @ last index 
@@ -36,7 +21,5 @@ def miniMaxSum(arr):
     print(mn,mx)
     
 if __name__ == '__main__':
-
     arr = list(map(int, input().rstrip().split()))
-
     miniMaxSum(arr)
--- LeetCode/PlusMinus.py
+++ LeetCode/PlusMinus.py
@@ -1,5 +1,5 @@
 """
-# Plus Minus 
+Plus Minus 
 Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero.
 Print the decimal value of each fraction on a new line with 6 places after the decimal.
 
@@ -57,7 +57,5 @@ def plusMinus(arr):
     
 if __name__ == '__main__':
     n = int(input().strip())
-
     arr = list(map(int, input().rstrip().split()))
-
     plusMinus(arr)
