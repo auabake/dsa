68f6d7e9b0fa8f7fad2bb6d3bf45872a3b85b6c2
Abubakar Njumwa <auabake@gmail.com>
2022-12-15 10:19:34 +0300

    Reverse Integer

---
+++ LeetCode/ReverseInteger.py
@@ -0,0 +1,41 @@
+"""
+Reverse Integer
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+
+Input: x = 123
+Output: 321
+
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+"""
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        x = str(x)
+        neg = 1
+        if x[0] == '-':
+            neg = -1
+            x = x[1:]
+        x = ''.join(reversed(x))
+        x = int(x) * neg
+        if not (-2**31 <= x <= 2 ** 31 - 1):
+            return 0
+        return x
